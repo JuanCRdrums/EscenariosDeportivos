bc393fec20e5904f6257f213ab117ef1a0e7fb69
hectormesa07 <hectormesa@utp.edu.co>
2019-06-01 02:54:39 -0500

    parte de front en un 90%

---
+++ Sprint1/front/Deportapp/mapa.php
@@ -0,0 +1,154 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>DeportApp</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="DeportApp - Escenarios Deportivos">
+	<meta name="keywords" content="DeportApp, Escenarios Deportivos, Futbol, Voleibol, UTP">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->   
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	
+	
+	
+	<!-- Header section start -->   
+	<header class="header-area">
+		
+		<div class="phone-number">DeportApp</div>
+		<nav class="nav-menu">
+			<ul>
+				<li ><a href="index.php">Inicio</a></li>
+				<li class="active"><a href="mapa.php"> Mapa </a></li>
+				<li ><a href="consultar.php"> Buscar Escenarios Deportivos</a></li>
+				<li><a href="administrar.php">Administrar Escenarios</a></li>
+			</ul>
+		</nav>
+	</header>
+	<!-- Header section end -->   
+
+
+	<!-- Page header section start -->
+	<section class="page-header-section set-bg" data-setbg="img/header.jpg">
+		<div class="container">
+			<h1 class="header-title">Mapa<span>.</span></h1>
+		</div>
+	</section>
+	<!-- Page header section end -->
+
+
+
+	<!-- Intro section start -->
+	<section class="intro-section spad">
+		<div class="container" align="center">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d23076.175149986695!2d-75.70117335340728!3d4.81205260886676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses!2sco!4v1559374689468!5m2!1ses!2sco" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</div>
+	</section>
+	<!-- Intro section end -->
+
+
+
+
+
+
+	<!-- Footer section start -->
+	
+	<footer class="footer-section">
+	
+		<div class="footer-social">
+		<br/><br/><br/><br/>
+			<div class="social-links">
+				<a href="#"><i class="fa fa-pinterest"></i></a>
+				<a href="#"><i class="fa fa-linkedin"></i></a>
+				<a href="#"><i class="fa fa-instagram"></i></a>
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-twitter"></i></a>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-9 offset-lg-3">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="footer-item">
+								<ul>
+									<li><a href="index.php">Inicio</a></li>
+									<li><a href="mapa.php">Mapa</a></li>
+									<li><a href="consultar.php">Buscar Escenarios Deportivos</a></li>
+									<li><a href="administrar.php">Administrar Escenarios</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="footer-item">
+								<ul>
+									<li><a href="#">Terminos y Condiciones</a></li>
+									<li><a href="#">Contactanos</a></li>
+									<li><a href="#">Soporte</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="footer-item">
+								<ul>
+									<li><a href="#">Héctor Mesa</a></li>
+									<li><a href="#">Felipe Bravo</a></li>
+									<li><a href="#">Camilo Rojas</a></li>
+									<li><a href="#">Sebastian Sanchez</a></li>
+									<li><a href="#">UTP</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+     <div class="copyright">Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved. </div>
+    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+
+	</footer>
+	<!-- Footer section end -->
+
+
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/isotope.pkgd.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.owl-filter.js"></script>
+	<script src="js/magnific-popup.min.js"></script>
+	<script src="js/circle-progress.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
